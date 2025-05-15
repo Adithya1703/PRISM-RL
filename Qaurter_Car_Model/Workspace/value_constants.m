18b37f12b72e508dfb75ca3e6c0ab5b289fec1b1
Adithya Sharma Mukurala <adithyasharma1703@gmail.com>
2025-05-15 15:33:16 +0530

    made changes to constants that are used based on refarences

--- Qaurter_Car_Model/Workspace/value_constants.m
+++ Qaurter_Car_Model/Workspace/value_constants.m
@@ -1,5 +1,10 @@
-kkk = 20000;
+kkk = 30000;
 ccc = 2500;
-msm_sms=290;
-mum_umu=59;
-kt=190000;
+msm_sms=300;
+mum_umu=47;
+kt=200000;
+ct = 300;
+
+% based on passenger cars like Toyota Corolla and Volkswagon Golf
+% curb weight of both is 1350 and 1400 and their unpsrung mass estimate is nealy 45-50kg
+% kkk, ccc, kt and ct are based on SAE technical papers - "Development of Full Vehicle Ride Model with Integrated Tire Model" and "Ride and Handling Analysis of a Passenger Car Using Quarter Car Model"
